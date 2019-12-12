f8a06570cc8f6952dcb1620de73ad27a57c1bfda
Bartosz Kalota <bartek.kalota@wp.pl>
2019-12-12 11:54:50 +0100

    3) Gallery with filter

---
+++ 3_GalleryWithFilter/js/app.js
@@ -0,0 +1,39 @@
+function init() {
+
+    const images = document.querySelectorAll('#gallery img');
+    const hideBtn = document.getElementById('hideButton');
+    const showBtn = document.getElementById('showButton');
+    const tagInput = document.getElementById('tagInput');
+    
+    function hideImages(event) {
+        for (let i = 0; i < images.length; i++) {
+            const phrase = tagInput.value.toLowerCase();
+            let imgDataTags = images[i].dataset.tag;
+            if ( imgDataTags.includes(phrase) ) {
+                images[i].classList.add('invisible');
+            }
+            else {
+                images[i].classList.remove('invisible');
+            }
+        }
+        tagInput.value = '';
+    }
+    hideBtn.addEventListener('click', hideImages);
+
+    function showImages(event) {
+       for (let i = 0; i < images.length; i++) {
+            const phrase = tagInput.value.toLowerCase();    
+            let imgDataTags = images[i].dataset.tag;
+            if ( imgDataTags.includes(phrase) ) {
+                images[i].classList.remove('invisible');
+            }
+            else {
+                images[i].classList.add('invisible');
+            }
+        }
+        tagInput.value = '';
+    }
+    showBtn.addEventListener('click', showImages);
+
+}
+document.addEventListener('DOMContentLoaded', init);
